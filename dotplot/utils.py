4b78b2ea7e9095bb59cafcb29e0004260a752d4d
jeffery <jeffery_cpu@163.com>
2021-04-13 16:29:48 +0800

    update clusterprofile utility

--- dotplot/utils.py
+++ dotplot/utils.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 DEFAULT_CLUSTERPROFILE_KEYS = {
-    'item_key': 'Description', 'group_key': 'group', 'sizes_key': 'GeneRatio', 'color_key': 'pvalue'
+    'item_key': 'Description', 'group_key': 'group', 'sizes_key': 'Count', 'color_key': 'pvalue',
+    'circle_key': 'qvalue'
 }
 
 
@@ -20,7 +21,7 @@ def merge_clusterprofile_results(dataframes, groups, group_key='group', term_lis
                 merged_df = pd.concat((merged_df, _sub_df))
             else:
                 merged_df = _sub_df
-    merged_df = merged_df[['Description', 'pvalue', 'GeneRatio', group_key]]
-    merged_df['GeneRatio'] = merged_df.GeneRatio.map(lambda x: int(x.split('/')[0]))
+    merged_df['qvalue'] = merged_df['qvalue'].map(lambda x: -np.log10(x))
     merged_df['pvalue'] = merged_df['pvalue'].map(lambda x: -np.log10(x))
+    merged_df['p.adjust'] = merged_df['p.adjust'].map(lambda x: -np.log10(x))
     return merged_df
