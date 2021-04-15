df753e3efebb278496108378ed10f9817ca177b5
jeffery <jeffery_cpu@163.com>
2021-04-15 15:59:19 +0800

    add setup files & update readme，tutorial

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Light Weighted dotplot drawer
 
 Examples of dotplot can be found [tutorial](https://nbviewer.jupyter.org/github/jefferyUstc/python-dotplot/blob/main/tutorial/tutorials.ipynb)
 
+# Installation
+```shell script
+pip install python_dotplot
+```
---
+++ setup.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_dotplot",
+    version="0.0.1a1",
+    author='jefferyUstc',
+    author_email="jeffery_cpu@163.com",
+    description="light weighted dotplot drawer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jefferyUstc/python-dotplot",
+    project_urls={
+        'python-dotplot': 'https://github.com/jefferyUstc/python-dotplot',
+    },
+    zip_safe=False,
+    keywords="dotplot python scatter",
+    python_requires='>=3.6',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        path.strip() for path in Path('requirements.txt').read_text('utf-8').splitlines()
+    ],
+    entry_points={},
+    package_data={},
+    include_package_data=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Natural Language :: English",
+    ],
+)
